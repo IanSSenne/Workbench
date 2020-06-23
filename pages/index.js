bfaf07d5582d278f8cb16db7399ca5ecdbbbcde3
IanSSenne <IanSSenne@gmail.com>
2020-06-23 02:09:19 -0700

    fix fatal build error

--- pages/index.js
+++ pages/index.js
@@ -2,13 +2,14 @@ import React from "react";
 import "../scss/index.scss";
 import Head from "next/head";
 import { TopBar } from "../components/topbar";
+import { Wrap } from "../components/wrap";
 export default (props) => {
   return (
-    <>
+    <Wrap>
       <Head>
         <style>{``}</style>
       </Head>
       <TopBar></TopBar>
-    </>
+    </Wrap>
   );
 };
